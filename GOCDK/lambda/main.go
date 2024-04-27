fdda6603a76f822523f9e2e5c3a19619f44fe098
Vivek Vishal <vishalvivek488@gmail.com>
2024-04-27 19:38:43 +0000

    ready and deploy lambda
    Signed-off-by: GitHub <noreply@github.com>

--- GOCDK/lambda/main.go
+++ GOCDK/lambda/main.go
@@ -1,16 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
 type MyEvent struct {
 	Username string `json:"username"`
 }
 
-func HandleRequest(event MyEvent) error{
+func HandleRequest(event MyEvent) (string, error){
 	if event.Username == "" {
-
+		return "",fmt.Errorf("username can't be empty")
 	}
-	return nil
+	return fmt.Sprintf("Successfully called by ~ %s", event.Username), nil
 }
 
 func main() {
-
+	lambda.Start(HandleRequest)
 }
