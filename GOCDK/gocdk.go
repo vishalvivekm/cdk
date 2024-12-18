a10fcbd24627f1066514b26dd933d0b913c89b38
Vivek Vishal <vishalvivek488@gmail.com>
2024-12-18 18:37:04 +0000

    add dynamodb
    Signed-off-by: GitHub <noreply@github.com>

--- GOCDK/gocdk.go
+++ GOCDK/gocdk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -19,13 +20,24 @@ func NewGocdkStack(scope constructs.Construct, id string, props *GocdkStackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("myLambdaFunc"),&awslambda.FunctionProps{
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY, // remove the DB with cdk destroy
+		
+	})
+	myFunction := awslambda.NewFunction(stack, jsii.String("myLambdaFunc"),&awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		// Code: awslambda.AssetCode_FromAssetImage()
 		// Code: awslambda.S3Code_FromBucket()
 		Code: awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+
+	table.GrantReadWriteData(myFunction)
 	// The code that defines your stack goes here
 
 	// example resource
