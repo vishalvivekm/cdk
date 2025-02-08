6a89fd6419eb5e222766898376738521265b40e7
Vivek Vishal <vishalvivek488@gmail.com>
2025-02-08 13:59:01 +0000

    route: /login
    Signed-off-by: GitHub <noreply@github.com>

--- GOCDK/lambda/main.go
+++ GOCDK/lambda/main.go
@@ -27,7 +27,9 @@ func main() {
 	lambda.Start(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 		switch request.Path {
 		case "/register":
-			return myApp.ApiHandler.RegisterUserHandler(request), nil
+			return myApp.ApiHandler.RegisterUserHandler(request)
+		case "/login":
+			return myApp.ApiHandler.LoginUser(request)
 		default:
 			return events.APIGatewayProxyResponse{
 			Body: "Not found",
